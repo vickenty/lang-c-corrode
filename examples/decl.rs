668ec00b9cc40737648a7e27706183ae1dab7823
Vickenty Fesunov <kent@setattr.net>
2018-01-02 22:12:45 +0100

    fmt

--- examples/decl.rs
+++ examples/decl.rs
@@ -7,8 +7,10 @@ fn main() {
     let text = env::args().nth(1).unwrap();
     let ast = lang_c::driver::parse_preprocessed(&Default::default(), text).unwrap();
     let alloc = &lang_c_corrode::Alloc::new();
-    let hir =
-        lang_c_corrode::interpret_translation_unit(alloc, &mut lang_c_corrode::Env::new(), &ast.unit)
-            .unwrap();
+    let hir = lang_c_corrode::interpret_translation_unit(
+        alloc,
+        &mut lang_c_corrode::Env::new(),
+        &ast.unit,
+    ).unwrap();
     println!("{:#?}", hir);
 }
